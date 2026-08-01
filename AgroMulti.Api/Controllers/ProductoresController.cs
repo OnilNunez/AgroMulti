b49b348879167d534b66bf4673de6b99e180e122
Elier Onil Núñez <elironil@icloud.com>
2026-08-01 09:29:08 -0400

    feat(api): crear AgroMulti.Api y configurar la base del proyecto

---
+++ AgroMulti.Api/Controllers/ProductoresController.cs
@@ -0,0 +1,28 @@
+﻿using AgroMulti.Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgroMulti.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductoresController : ControllerBase
+{
+    private readonly AgroMultiContext _context;
+
+    public ProductoresController(AgroMultiContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var productores = await _context.Productors
+            .AsNoTracking()
+            .OrderBy(p => p.Nombre)
+            .ToListAsync();
+
+        return Ok(productores);
+    }
+}
---
+++ AgroMulti.Api/Program.cs
@@ -0,0 +1,32 @@
+using AgroMulti.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AgroMultiContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AgroMultiConnection"));
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
